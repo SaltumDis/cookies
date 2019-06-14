0eb4f3b91592cc15a95b9d3bb683c9feca799804
SaltumDis <varatinskaite.adelina@gmail.com>
2019-06-14 12:07:54 +0300

    TEST

---
+++ remove_merged_git_branches.sh
@@ -0,0 +1,9 @@
+set -e
+  git fetch --all --prune
+  # Remove local fully merged branches
+git branch --merged upstream/master | sed 's/^[ *]\+//' | grep -v '^master$' | xargs -r -d'\n' git
+  branch -D
+  # Remove origin/ fully merged branches, except for master
+git branch --remotes --merged upstream/master | sed 's/^[ *]\+//' | grep '^origin/' | grep -v
+           '/master$|' | sed 's/^origin\///' | xargs -r -d'\n' git push --delete origin
+git fetch --all --prune
