e30a8f6809e6905b2c72c179e7f6312b4afc0a40
SaltumDis <varatinskaite.adelina@gmail.com>
2019-06-14 12:27:28 +0300

    LAL

--- remove_merged_git_branches.sh
+++ remove_merged_git_branches.sh
@@ -1,9 +1,12 @@
 set -e
   git fetch --all --prune
   # Remove local fully merged branches
-git branch --merged upstream/master | sed 's/^[ *]\+//' | grep -v '^master$' | xargs -r -d'\n' git
+git branch --merged master | sed 's/^[ *]\+//' | grep -v '^master$' | xargs -r -d'\n' git
   branch -D
   # Remove origin/ fully merged branches, except for master
-git branch --remotes --merged upstream/master | sed 's/^[ *]\+//' | grep '^origin/' | grep -v
+git branch --remotes --merged master | sed 's/^[ *]\+//' | grep '^origin/' | grep -v
            '/master$|' | sed 's/^origin\///' | xargs -r -d'\n' git push --delete origin
 git fetch --all --prune
+
+# Remove remote fully merged branches into release.
+git branch --merged master| egrep -v "(^\*|master|release)" | sed 's/origin\///' | xargs -n 1 git push origin
